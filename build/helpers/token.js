b56016a2bce2ee6ce5a2d99adf4cd62b2cddb826
Jon Russell <jon@jonrussell.xyz>
2021-05-24 10:49:51 -0500

    removed build dir

--- build/helpers/token.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const jsonwebtoken_1 = require("jsonwebtoken");
-const config_1 = require("../config");
-function makeJWT(user) {
-    let payload = { username: user };
-    return jsonwebtoken_1.sign(payload, config_1.SECRET_KEY);
-}
-exports.default = makeJWT;
